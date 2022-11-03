03580747c3b316ba570e6c5b174cafb1f462bf56
Midhun <midhunmanu384@gmail.com>
2022-11-03 19:01:52 +0530

    counts calories correctly

---
+++ src/main/java/com/calculations/caloriecounter/MainActivity.java
@@ -0,0 +1,82 @@
+package com.calculations.caloriecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    EditText editText;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.getCalories);
+        editText = findViewById(R.id.input_food);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(new getCalorieValue().execute().getStatus().toString());
+            }
+        });
+    }
+
+    private class getCalorieValue extends AsyncTask<Void , Void , Void> {
+        String output;
+        String input;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            input = editText.getText().toString();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String url = "https://www.google.com/search?q=%s+calories";
+                String product = input;
+                String URL = String.format(url, product);
+                Document document = Jsoup.connect(URL).get();
+                Elements cals = document.select("div[aria-live]");
+                ArrayList<String>calories = new ArrayList<>();
+                for(Element i : cals) {
+                    calories.add(i.text());
+                }
+                output = calories.get(1);
+            }
+            catch (IOException e) {
+                Toast.makeText(MainActivity.this , e.toString() , Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            textView.setText("number of calories : " + output);
+        }
+    }
+}
